9777fdd1a4ba147fa18dbe758e4b2589d1aee81b
ALOLA SHALOM <alolashalom@yahoo.com>
2022-08-15 15:19:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this!")
+print ("This is fun!")
+print ("Yay! Printing")
+print ("I'd much rather 'you not'")
+print ('I "said" do not touch this!')
+print ("Make your script print another line")
+#print ('Put an octothorpe/hash at beginning of the line')
+
+#Exercise 2
+print ('I will now count my chickens:')
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+print ('Now I will count the eggs')
+print (3 + 2 + 1 - 5 + 4 % 2- 1 / 4 + 6)
+print ("Is it true that 3 + 2 < 5 - 7?")
+print (3 +2 < 5 -7)
+print ('"what is 3 + 2?", 3 + 2')
+print ('What is "5 - 7" 5-7')
+print ("Oh! that's why it's false")
+print ("How about some more")
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
+
+#Exercise 3
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print ("There are", cars, "cars available")
+print ("There are", drivers, "drivers available")
+print ("There will be", cars_not_driven, "empty cars today")
+print ("We can transport", carpool_capacity, "people today")
+print ("We need to put about", average_passengers_per_car, "in each car")
+
+#Exercise 3
+my_name = 'Shalom Boluwatife'
+my_age = 35
+my_weight = 74
+my_height = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print ("Let's talk about %s")
+print ("He's %d inches tall")
+print ("He's %d pound heavy")
+print ("Actually that's not too heavy")
+print ("He's got %s eyes and %s hair")
+print ("His teeth are usually %s depending on the coffee")
